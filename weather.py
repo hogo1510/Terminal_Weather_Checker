49305b1126b3640ccf21eeb42ba71177b6ef79c3
Lucas Fenstra <55227884+hogo1510@users.noreply.github.com>
2024-01-24 14:33:12 +0100

    The Basics are done

--- weather.py
+++ weather.py
@@ -1,15 +1,37 @@
 #put in the weather API
 import requests
+import json
 
 Weather_URL = "http://api.weatherapi.com/v1/current.json"
-Weather_API = "Your key"
-req = requests.get(Weather_URL)
-print("Checking... \nCode is: ", req.status_code)
-#functions for al de opties
-def Main ():
-    #Options
+Weather_API = "API_KEY"
 
-    Locatie = input("In welke Locatie bevind u zich?")
+#functions for al de opties
+def Main (URL, API):
+    #Options
+    Locatie = input("In welke Locatie bevind u zich? :")
+
+    para = {
+    "key": API,
+    "q": Locatie
+    }
+
+    req = requests.get(URL, params=para)
+    data = req.json()
+    current = data["current"]
+
+    print("Temperatuur is nu : ", current["temp_c"], "Celsius")
+    print("Percentage wolken is : ", current["cloud"],"%")
+    print("Wind : ",current["wind_kph"], "Km per uur")
+    print("Directie van de wind : ",current["wind_dir"])
+
+req = requests.get(Weather_URL, Weather_API)
+print("Checking... \nCode is: ", req.status_code)
+
+if (req.status_code == 401):
+    print("conectie gemaakt!")
+    Main(Weather_URL, Weather_API)
+else:
+    raise Exception("\n Oops... er is geen conectie") 
 
 
 
